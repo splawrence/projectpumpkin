cea7b2990f8297748f206ed57fc9a0aebd3e8b70
Stephen Lawrence <sp_lawrence@outlook.com>
2024-01-02 20:01:35 -0600

    init

---
+++ src/main/java/com/splawrence/projectpumpkin/ProjectpumpkinApplication.java
@@ -0,0 +1,13 @@
+package com.splawrence.projectpumpkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpumpkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpumpkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splawrence/projectpumpkin/ProjectpumpkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splawrence.projectpumpkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpumpkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
